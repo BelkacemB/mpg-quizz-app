f77d68d405e0a4b12b88a60aa79922952565ca19
Belkacem Berchiche <b_berchiche@esi.dz>
2021-10-02 17:17:51 +0200

    Add functional radar chart

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { LoadingIndicator } from './LoadingIndicator';
 import ReactTooltip from 'react-tooltip';
 import { getPositionWeightedColor, defaultLabelStyle } from './styles/styles'
 import { PieChart } from 'react-minimal-pie-chart';
-
+import { Radar } from 'react-chartjs-2';
 
 function App() {
 
@@ -30,6 +30,7 @@ function App() {
 
   const [suggestedTeam, setSuggestedTeam] = useState([])
   const [expenseData, setExpenseData] = useState([])
+  const [teamAnalytics, setTeamAnalytics] = useState({})
 
   const biddingMarks = [
     {
@@ -76,6 +77,7 @@ function App() {
   const handlePrefSubmit = () => {
     setSuggestedTeam([])
     setExpenseData([])
+    setTeamAnalytics([])
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
@@ -107,6 +109,17 @@ function App() {
     return depPlayers.reduce((preValue, curValue) => preValue + curValue.bid, 0)
   };
 
+  const computeAggScoreForDepartmentAndCriteria = (team, criteria, department = undefined) => {
+    if (department) {
+      team = team.filter(p => p.mpg_position === department)
+    }
+    let score = team.reduce((preValue, currentValue) => preValue + currentValue[criteria], 0) / team.length
+    if (['xG', 'team_xG_against'].includes(criteria)) {
+      score *= 18
+    }
+    return score
+  }
+
   useEffect(() => {
     if (suggestedTeam.length > 0) {
       setExpenseData([
@@ -115,8 +128,30 @@ function App() {
         { title: 'Defence', value: getDepartmentTotal(suggestedTeam, 'D'), color: getPositionWeightedColor(0.8, 'D')['background-color'] },
         { title: 'Goalkeepers', value: getDepartmentTotal(suggestedTeam, 'G'), color: getPositionWeightedColor(0.8, 'G')['background-color'] }
       ])
+      // TODO Do a custom scale 
+      setTeamAnalytics({
+        labels: ['Attack', 'Midfield', 'Defence'],
+        datasets: [
+          {
+            label: "Team strengths",
+            data: [
+              computeAggScoreForDepartmentAndCriteria(suggestedTeam, 'average', 'A'),
+              computeAggScoreForDepartmentAndCriteria(suggestedTeam, 'average', 'M'),
+              computeAggScoreForDepartmentAndCriteria(suggestedTeam, 'average', 'D')],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+          }
+        ]
+      })
     }
   }, [suggestedTeam])
+
+  const radarChartOptions = {
+    scale: {
+      ticks: { beginAtZero: true },
+    },
+  };
 
 
   return (
@@ -239,62 +274,67 @@ function App() {
           </Button>
         </FormControl>
 
-        <LoadingIndicator className="mx-8"/>
-        {suggestedTeam.length > 0 && (          
-        <div id="results" className="flex">
-          <div>
-            <h2><strong>Suggested team</strong></h2>
+        <LoadingIndicator className="mx-8" />
+        {suggestedTeam.length > 0 && (
+          <div id="results" className="flex">
+            <div>
+              <h2><strong>Suggested team</strong></h2>
 
 
-            <table className="table-auto mx-6 border-2 border-double border-gray rounded-lg">
-              <thead>
-                <tr>
-                  <th className="mx-6">Player</th>
-                  <th>Price</th>
-                  <th>Bid</th>
-                </tr>
-              </thead>
-              <tbody>
-                {suggestedTeam.length > 0 &&
-                  suggestedTeam.map(player => (
-                    // TODO Add solid borders between according to position 
-                    <tr key={player.player_name} className="border-2">
-                      <td >
-                        <ReactTooltip id={`playerTooltip${player.player_name}`} type='info'>
-                          <span>{`Team: ${player.Team}, games: ${player.games}, goals: ${player.goals}, assists: ${player.assists}, xG: ${player.xG.toFixed(2)}, MPG average rating: ${player.average}`}</span>
-                        </ReactTooltip>
-                        <span data-tip data-for={`playerTooltip${player.player_name}`}>{player.player_name}</span>
-                      </td>
-                      <td className="text-center"> {player.price} </td>
-                      <td className="text-center" style={getPositionWeightedColor(Math.min(1, player.bid / (player.price * 3)), player.mpg_position)}> <strong>{player.bid}</strong> </td>
-                    </tr>
-                  )
-                  )
-                }
-                <tr className="total-row">
-                  <td className="text-center"><strong>Σ</strong></td>
-                  <td className="text-center">{suggestedTeam.length > 0 ? suggestedTeam.reduce(((a, b) => a + b.price), 0) : 0}</td>
-                  <td className="text-center">{suggestedTeam.length > 0 ? suggestedTeam.reduce(((a, b) => a + b.bid), 0) : 0}</td>
-                </tr>
-              </tbody>
-            </table>
-            <span>Team MPG rating: <b>{suggestedTeam.length > 0 ? (suggestedTeam.reduce(((a, b) => a + b.average), 0) / suggestedTeam.length).toFixed(2) : 0}</b></span>
-          </div>
-          <div >
-            <h2><strong>Total bids per department</strong></h2>
-            <br />
-            {expenseData.length > 0 &&
-              <PieChart
-                data={expenseData}
-                style={{ height: '250px' }}
-                label={({ dataEntry }) => `${(dataEntry.value * 100) / 500}%`}
-                labelStyle={{ ...defaultLabelStyle }}
-                segmentsStyle={{ transition: 'stroke .3s' }}
-                segmentsShift={1}
-                className="mx-6"
-              />}
-          </div>
-        </div>)}
+              <table className="table-auto mx-6 border-2 border-double border-gray rounded-lg">
+                <thead>
+                  <tr>
+                    <th className="mx-6">Player</th>
+                    <th>Price</th>
+                    <th>Bid</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {suggestedTeam.length > 0 &&
+                    suggestedTeam.map(player => (
+                      // TODO Add solid borders between according to position 
+                      <tr key={player.player_name} className="border-2">
+                        <td >
+                          <ReactTooltip id={`playerTooltip${player.player_name}`} type='info'>
+                            <span>{`Team: ${player.Team}, games: ${player.games}, goals: ${player.goals}, assists: ${player.assists}, xG: ${player.xG.toFixed(2)}, MPG average rating: ${player.average}`}</span>
+                          </ReactTooltip>
+                          <span data-tip data-for={`playerTooltip${player.player_name}`}>{player.player_name}</span>
+                        </td>
+                        <td className="text-center"> {player.price} </td>
+                        <td className="text-center" style={getPositionWeightedColor(Math.min(1, player.bid / (player.price * 3)), player.mpg_position)}> <strong>{player.bid}</strong> </td>
+                      </tr>
+                    )
+                    )
+                  }
+                  <tr className="total-row">
+                    <td className="text-center"><strong>Σ</strong></td>
+                    <td className="text-center">{suggestedTeam.length > 0 ? suggestedTeam.reduce(((a, b) => a + b.price), 0) : 0}</td>
+                    <td className="text-center">{suggestedTeam.length > 0 ? suggestedTeam.reduce(((a, b) => a + b.bid), 0) : 0}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <span>Team MPG rating: <b>{suggestedTeam.length > 0 ? (suggestedTeam.reduce(((a, b) => a + b.average), 0) / suggestedTeam.length).toFixed(2) : 0}</b></span>
+            </div>
+            <div >
+              <h2><strong>Total bids per department</strong></h2>
+              <br />
+              {expenseData.length > 0 &&
+                <div>
+                  <PieChart
+                    data={expenseData}
+                    style={{ height: '250px' }}
+                    label={({ dataEntry }) => `${(dataEntry.value * 100) / 500}%`}
+                    labelStyle={{ ...defaultLabelStyle }}
+                    segmentsStyle={{ transition: 'stroke .3s' }}
+                    segmentsShift={1}
+                    className="mx-6"
+                  />
+                  {teamAnalytics && (
+                    <Radar data={teamAnalytics} options={radarChartOptions} />)}
+                </div>}
+
+            </div>
+          </div>)}
       </div>
 
       <div>
